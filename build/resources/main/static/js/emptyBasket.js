476be7f0b79adb6d4752677770b0223853b77164
lauralikespi <laura.gemmell012@gmail.com>
2017-06-08 09:39:49 +0100

    server and yoti working separately

--- build/resources/main/static/js/emptyBasket.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	if( !$.trim( $('.displayBasket').html() ).length ) {
-		$('.itemsNumber').text("Empty");
-		$('.displayBasket').text("Your basket is empty!");
-	}
-})
---
+++ yoti/src/main/java/com/yoti/api/examples/springboot/YotiSDKSampleApp.java
@@ -0,0 +1,13 @@
+package com.yoti.api.examples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YotiSDKSampleApp {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(YotiSDKSampleApp.class, args);
+    }
+
+}
